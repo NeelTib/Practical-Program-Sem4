b1b15d223a96db4bc50d38e1ff2328fcafb71d59
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-11-25 20:52:15 +0530

    2b

---
+++ Practical 2/2b.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#include <conio.h>
+#include <graphics.h>
+void main()
+{
+
+    // Declaring the variables and Initliazing the graph
+    int gdriver = DETECT, gmode;
+    initgraph(&gdriver, &gmode, "");
+
+
+
+
+    getch();
+    closegraph();
+}
