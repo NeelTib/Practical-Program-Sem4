ca4cdb05c0cf5f1817a0efc93c9e15af59d5ff83
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2023-02-07 12:31:29 +0530

    Update prac9c.java

--- Core Java/prac9c.java
+++ Core Java/prac9c.java
@@ -1,13 +1,11 @@
-public class prac9a{
+public class prac9a {
     public static void main(String[] args) {
-        try{
-            float a=10,b=0,c;
-            c=a/b;
-            System.out.println("The value of C is: "+c);
-
-        }
-        catch(ArithmeticException e){
-            System.out.println("Exception Raised: "+e);
+        try {
+            int a = 10, b = 0, c;
+            c = a / b;
+            System.out.println("The value of C is: " + c);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed: " + e);
         }
     }
 }
